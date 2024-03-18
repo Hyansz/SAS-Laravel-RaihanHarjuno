e01a39e7d6e924c2632a8ce599b1fc135f7f3b9f
Raihan Regita Harjuno <raihanregita.ppqita@gmail.com>
2024-03-18 10:25:27 +0700

    SAS LARAVEL

--- resources/views/pages/kontak.blade.php
+++ resources/views/pages/kontak.blade.php
@@ -5,9 +5,9 @@
         <div class="col-md-6 text-center col-xl-12">
             <h2>C O N T A C T</h2>
             <div class="num">
-                <a href="/" class="btn p-0">0856 6668 114</a>
+                <a href="http://wa.me/08566668114" class="btn p-0">0856 6668 114</a>
                 <br>
-                <a href="/" class="btn p-0">0895 3915 39188</a>
+                <a href="http://wa.me/0895391539188" class="btn p-0">0895 3915 39188</a>
             </div>
             <p>Ruko King Plasa Blok D No.1 Batu Aji, Kota Batam</p>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.1541262905635!2d103.97963562496524!3d1.0454772489444661!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d98d536404a375%3A0x21c33a99dfe53b6e!2sRuko%20king%20plaza!5e0!3m2!1sen!2sid!4v1710729564085!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-5"></iframe>
